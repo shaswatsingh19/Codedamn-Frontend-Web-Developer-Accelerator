23e18b02da57311edc4a2ec4d619222e43621d13
shaswat singh <shaswatsingh.ose@gmail.com>
2022-07-13 23:56:10 +0530

    minor updates

--- 04.ToDoApp/index.js
+++ 04.ToDoApp/index.js
@@ -21,17 +21,50 @@ addBtn.addEventListener('click', (e)=> {
     }else{
         console.log(todo)
         addTodo(todo)
+        addInput.value = ''
+        updateTodo()
     }
 })
+
 
 
 function addTodo(todo){
     let todoTask  = ` 
                 <div class="task">
-					<input type="text" id="added-task" name='todo' value="${todo}">
-					<input type="button" value="✏️" name='rename' id="rename-task">
-					<input type="button" value="❌" name='delete' id="del-task">
+					<input type="text" id="added-task" name='todo' disabled value="${todo}">
+					<input type="button" value="✏️" name='rename' class="rename-task">
+					<input type="button" value="❌" name='delete' class="del-task">
 				</div>
                 ` 
     newTasks.innerHTML += todoTask
+
+}
+
+
+function updateTodo(){
+    
+
+    let task  = document.querySelectorAll('.task')
+
+    task.forEach((t) => {
+        console.log(t)
+        t.addEventListener('click', e =>{
+
+            if(e.target.classList.contains('rename-task')){
+                
+                console.log('rename')
+                
+            }else {
+                console.log('del')
+            }
+        })
+
+    })
+    // renameBtn.addEventListener('click',(e)=>{
+    //     console.log(e)
+    // })
+    
+    // delBtn.addEventListener('click',(e)=>{
+    //     console.log(e)
+    // })
 }
